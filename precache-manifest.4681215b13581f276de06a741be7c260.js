442a837482a19003324c2e284d13fe3f9f2a868c
“mkczarkowski” <marcinczarkowski@onet.eu>
2019-12-21 10:37:11 +0100

    Updates

---
+++ precache-manifest.4681215b13581f276de06a741be7c260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6c819e52df1076edc7859688a348d7",
+    "url": "/opanuj-js-deployment/index.html"
+  },
+  {
+    "revision": "5654b1daecd8694b91b0",
+    "url": "/opanuj-js-deployment/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7608d8d77c333428362d",
+    "url": "/opanuj-js-deployment/static/js/2.f8a9237c.chunk.js"
+  },
+  {
+    "revision": "5654b1daecd8694b91b0",
+    "url": "/opanuj-js-deployment/static/js/main.69924298.chunk.js"
+  },
+  {
+    "revision": "8a1118e4a182ac504fb1",
+    "url": "/opanuj-js-deployment/static/js/runtime-main.2f3b4053.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/opanuj-js-deployment/static/media/logo.25bf045c.svg"
+  }
+]);
